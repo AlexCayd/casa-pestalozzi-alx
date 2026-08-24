782036a802a0560e1cf6e01fe6203b639b8265ec
Leonardo Velasco Ojeda <leonardo.velasco.oj@gmail.com>
2026-08-24 11:02:00 -0600

    migraciones eliminadas

--- database/migrations/2026_08_20_reservaciones_motivo_cancelacion.sql
+++
@@ -1,3 +0,0 @@
--- Motivo administrativo de cancelación. Forward-only: no modifica migraciones previas.
-ALTER TABLE reservaciones
-  ADD COLUMN motivo_cancelacion VARCHAR(500) NULL AFTER comentario_admin;
